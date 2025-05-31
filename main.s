aa403addc8b98dd18ade39f9fd692db665281575
Oscar Rompich <oscarligo6@gmail.com>
2025-05-31 16:57:00 -0600

    Estructura del Proyecto

---
+++ main.s
@@ -0,0 +1,44 @@
+
+/*********************************************************
+* @file      main.s
+* @author    Ivan Morataya
+*			 Juan Gualim
+*			 Oscar Rompich
+* @date      June, 2025
+* @brief     Secuencia de 8 bits 
+* @details
+*			(descripcion del proyecto)
+**********************************************************/
+
+/*==================
+    DEFINICIONES
+====================*/
+
+
+
+
+
+
+.syntax unified
+.cpu cortex-m4
+.fpu softvfp
+.thumb
+
+.section .text
+.global __main
+
+__main:
+
+
+
+
+end:
+	B end
+
+
+.section .data
+
+
+
+	.align
+	.end
